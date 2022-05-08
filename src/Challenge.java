5d6980cfa425b9469136572a140cd53b6028c314
berkaycanozcelik <brkycn10@gmail.com>
2022-05-08 23:17:02 +0300

    added solution

--- src/Challenge.java
+++ src/Challenge.java
@@ -1,40 +1,27 @@
 public class Challenge {
-    static int larrgestPalindrome(int n)
-    {
-        int upperLimit = ((int) Math.pow(10, n)) - 1;
-        int lowerLimit = ((int) Math.pow(10, (n - 1)));
 
-        int maxResult = 0;
+    static int smallestMultiple(int x){
+        boolean isSmallestMultipeFound =false;
+        int number = 0;
 
-      for (int i = upperLimit; i >= lowerLimit; i--){
-          for (int j = i; j >= lowerLimit; j--){
-              int result = i * j;
+        while(!isSmallestMultipeFound){
+            number++;
+            for (int i=1; i <=x; i++){
+                if ( number % i != 0)
+                    break;
+                if (i == x){
+                    isSmallestMultipeFound = true;
+                }
 
-              if (result < maxResult)
-                  break;
-
-              int num = result;
-              int reverse = 0;
-
-                  while(num > 0){
-
-
-                      int kalan = num % 10;
-                      reverse = ( reverse * 10 ) + kalan;
-                      num /= 10;
-                  }
-
-              if (reverse == result && result >maxResult)
-                  maxResult = result;
-          }
-      }
-      return maxResult;
+            }
+        }
+        return number;
     }
 
-    // Driver code
+
     public static void main (String[] args)
     {
-        int n = 3;
-        System.out.print(larrgestPalindrome(n));
+       int n = 20;
+        System.out.println(smallestMultiple(n));
     }
 }
