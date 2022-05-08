6bcfdb40280e11e28e402f1f4f7d0633a9d9b096
berkaycanozcelik <brkycn10@gmail.com>
2022-05-08 23:27:38 +0300

    added solution

--- src/Challenge.java
+++ src/Challenge.java
@@ -1,27 +1,17 @@
 public class Challenge {
-
-    static int smallestMultiple(int x){
-        boolean isSmallestMultipeFound =false;
-        int number = 0;
-
-        while(!isSmallestMultipeFound){
-            number++;
-            for (int i=1; i <=x; i++){
-                if ( number % i != 0)
-                    break;
-                if (i == x){
-                    isSmallestMultipeFound = true;
-                }
-
-            }
-        }
-        return number;
-    }
-
-
     public static void main (String[] args)
     {
-       int n = 20;
-        System.out.println(smallestMultiple(n));
+        int n = 100;
+        int sumOfSquares = 0 ;
+        double sum= 0;
+
+        for (int i = 1; i <= n; i++){
+            sumOfSquares += Math.pow(i,2);
+            sum += i;
+        }
+
+        int result = (int)Math.pow(sum,2)- sumOfSquares;
+
+        System.out.println(result);
     }
 }
